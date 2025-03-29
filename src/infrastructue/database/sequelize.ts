7033bd5a817028aabfbf72e0c80fc7fce904ff09
LuisRicado77 <luissayas2018@gmail.com>
2025-03-28 22:33:21 -0500

    updating the ssl

--- src/infrastructue/database/sequelize.ts
+++ src/infrastructue/database/sequelize.ts
@@ -1,4 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
+import fs from "fs"; // Necesario para leer el certificado
+
 import { ContractModel } from "./models/contract.model";
 import { ReviewModel } from "./models/review.model";
 import { RolModel } from "./models/rol.model";
@@ -10,19 +12,25 @@ import { UserModel } from "./models/user.model";
 import { FavoritesModel } from "./models/favorites.model";
 import { ApplicationModel } from "./models/application.model";
 
-
-
 const sequelize = new Sequelize({
   host: 'projectsena.mysql.database.azure.com',
   port: 3306,
   database: 'senabd',
-  dialect: 'mysql', 
+  dialect: 'mysql',
   username: 'luis',
-  password: '3015916341Ricardo.',
-  models: [ApplicationModel,UserModel,ContractModel,ReceiptModel, RolModel, RentalModel,PropertyModel, PaymentModel,FavoritesModel,ReviewModel], 
-  logging: console.log
+  password: '3015916341Ricardo.', // ⚠ No es seguro dejar la contraseña en el código, usa variables de entorno
+  models: [
+    ApplicationModel, UserModel, ContractModel, ReceiptModel,
+    RolModel, RentalModel, PropertyModel, PaymentModel,
+    FavoritesModel, ReviewModel
+  ],
+  logging: console.log,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
-
-
 
 export default sequelize;
